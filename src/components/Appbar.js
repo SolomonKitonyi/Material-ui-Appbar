fc598eec97cc041f04b52d0aa4625861a7182bd5
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-03-30 02:19:04 +0300

    add new style

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
@@ -6,7 +7,9 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import Home from '@material-ui/icons/Home';
+import { InfoOutlined } from '@material-ui/icons';
+
 
 const Appbar = ()=>{
     const useStyles = makeStyles((theme) => ({
@@ -25,13 +28,16 @@ const Appbar = ()=>{
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
+          <IconButton edge="start" className={classes.menuButton} aria-label="menu">
+            <Link to="/Streams">
+              <Home className="classes.menuButton" fontSize="large" variant="filled"/>
+            </Link>
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
+            Welcome
           </Typography>
           <Button color="inherit">Login</Button>
+          <Button color="inherit">SignUp</Button>
         </Toolbar>
       </AppBar>
     </div>
